64f924ef3c3774255dca56e28fd709c718fd3183
Cee06-206 <ceciliapluma613@gmail.com>
2024-10-17 16:29:38 +0800

    program1.cpp

---
+++ images/PROGRAM5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int exitTime, entryTime, period;
+    float charge;
+    cout<<"Please enter yoour entry time: ";
+    cin>>ecitTime;
+
+    period = exitTime - entryTime;
+    if(period > 1)
+        charge = (period) + 1;
+    else 
+        charge = 2;
+
+    cout<<"The charge for your parking is "<<charge;
+}
